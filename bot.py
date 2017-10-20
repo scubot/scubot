4f1b51c99b3e70f22f9df3f1c1534f069f648e0f
Anson <git@anson-cheung.com>
2017-10-20 21:35:18 +0800

    Add basic discord bot functionality

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!ping'):
+        msg = 'Pong! This bot is alive.'
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print('User:',client.user.name)
+    print('ID',client.user.id)
+    print('------')
+
+client.run('put_your_token_here')
