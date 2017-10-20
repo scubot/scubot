3e07e509fe0b1dd7c02b39c490c994e3c0bb94b5
QuickRecon <aren.leishman@gmail.com>
2017-10-20 22:47:34 +0800

    Add unit conversion for ft

--- bot.py
+++ bot.py
@@ -1,6 +1,17 @@
+# TODO Put an enum matching unit pairs, will make code cleaner
+
 import discord
+import re
+from unitconverter import *
 
 client = discord.Client()
+
+
+def construct_response(messageregex):
+    string = messageregex.string + ' is '
+    currentvalue = int(messageregex.string.replace(messageregex.group(2), ''))
+    convertedvalue = feet_to_meters(currentvalue)
+    return string + str(convertedvalue)
 
 
 @client.event
@@ -11,6 +22,11 @@ async def on_message(message):
         msg = 'Pong! This bot is alive.'
         await client.send_message(message.channel, msg)
 
+    # this technique can be used for the other units
+    if re.search('[0-9]+(| )(ft|feet)', message.content) is not None:
+        msg = construct_response(re.search('[0-9]+(| )(ft|feet)', message.content)) + ' meters'
+        await client.send_message(message.channel, msg)
+
 
 @client.event
 async def on_ready():
@@ -18,6 +34,7 @@ async def on_ready():
     print('User:', client.user.name)
     print('ID', client.user.id)
     print('------')
+
 
 tokenfile = open('token')
 token = tokenfile.read().replace('\n', '')
