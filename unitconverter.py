5547a0f6b2a2a16299cc49107feb93bd1144fb10
QuickRecon <aren.leishman@gmail.com>
2017-10-20 22:12:41 +0800

    Add unitconverter functions

---
+++ unitconverter.py
@@ -0,0 +1,26 @@
+def meters_to_feet(meters):
+    return meters * 3.28084
+
+
+def feet_to_meters(feet):
+    return feet / 3.28084
+
+
+def kilograms_to_pounds(kilograms):
+    return kilograms * 2.20462
+
+
+def pounds_to_kilograms(pounds):
+    return pounds / 2.20462
+
+
+def bar_to_psi(bar):
+    return bar * 14.5038
+
+
+def psi_to_bar(psi):
+    return psi / 14.5038
+
+
+def fathoms_to_meters(fathoms):
+    return fathoms * 1.8288
