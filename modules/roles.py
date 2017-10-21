b8a80c8d3b78efd2367e332487e840bd123dade7
Anson <git@anson-cheung.com>
2017-10-21 22:49:18 +0800

    Add full functionality of adding roles

--- modules/roles.py
+++ modules/roles.py
@@ -4,8 +4,15 @@ import shlex
 rolesTriggerString = '!role'
 
 async def parse_roles_command(message, client):
+    server_roles = message.server.roles
+    server_roles_str = [x.name for x in server_roles]
     msg = shlex.split(message.content)
-    if len(msg) != 1
-        await client.send_message(message.channel, msg[1])
+    role = [i for i,x in enumerate(server_roles_str) if x == msg[1]]
+    if len(msg) != 1:
+        try:
+            await client.add_roles(message.author,message.server.roles[role[0]])
+        except discord.DiscordException:
+            msg = "I'm sorry " + message.author.name + " ,I'm afraid I can't do that."
+            client.send_message(message.channel, msg)
     else:
-        break
+        pass
