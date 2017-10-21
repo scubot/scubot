3fbfe633899b9e95534ff612a3f509ba0c1a1a65
Anson <git@anson-cheung.com>
2017-10-21 22:57:18 +0800

    Add comments

--- modules/roles.py
+++ modules/roles.py
@@ -1,13 +1,13 @@
 import discord
 import shlex
 
-rolesTriggerString = '!role'
+rolesTriggerString = '!role' # String to listen for as trigger
 
 async def parse_roles_command(message, client):
-    server_roles = message.server.roles
-    server_roles_str = [x.name for x in server_roles]
+    server_roles = message.server.roles # Grab a list of all roles as Role objects
+    server_roles_str = [x.name for x in server_roles] # String-ify it into their names
     msg = shlex.split(message.content)
-    role = [i for i,x in enumerate(server_roles_str) if x == msg[1]]
+    role = [i for i,x in enumerate(server_roles_str) if x == msg[1]] # Check where in the list the role is
     if len(msg) != 1:
         try:
             await client.add_roles(message.author,message.server.roles[role[0]])
