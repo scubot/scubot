2f2f18abbcde94e61c38a519b3b8d959be2e0301
Anson <git@anson-cheung.com>
2017-10-21 21:54:43 +0800

    Modify to use shlex for getting command arguments

--- modules/roles.py
+++ modules/roles.py
@@ -1,7 +1,11 @@
 import discord
+import shlex
 
 rolesTriggerString = '!role'
 
 async def parse_roles_command(message, client):
-    msg = 'Role!'
-    await client.send_message(message.channel, msg)
+    msg = shlex.split(message.content)
+    if len(msg) != 1
+        await client.send_message(message.channel, msg[1])
+    else:
+        break
