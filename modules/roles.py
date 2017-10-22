b142754e812c3d5154d80d9ad20e4eec3b319ca5
QuickRecon <aren.leishman@gmail.com>
2017-10-22 20:07:50 +0800

    Change to object orientation for modules

--- modules/roles.py
+++ modules/roles.py
@@ -1,24 +1,33 @@
 import discord
 import shlex
+from modules.botModule import BotModule
 
-rolesTriggerString = '!role' # String to listen for as trigger
 
-async def parse_roles_command(message, client):
-    server_roles = message.server.roles # Grab a list of all roles as Role objects
-    server_roles_str = [x.name for x in server_roles] # String-ify it into their names
-    msg = shlex.split(message.content)
-    role = [i for i,x in enumerate(server_roles_str) if x == msg[1]] # Check where in the list the role is
-    role_to_assign = message.server.roles[role[0]]
-    if len(msg) != 1:
-        try:
-            if role_to_assign in message.author.roles:
-                await client.remove_roles(message.author,role_to_assign)
-                msg = ":ok_hand: Removed you from " + role_to_assign.name + " ."
-            else:
-                await client.add_roles(message.author,role_to_assign)
-                msg = ":ok_hand: Added you to " + role_to_assign.name + " ."
-        except discord.DiscordException:
-            msg = "I'm sorry " + message.author.name + " ,I'm afraid I can't do that."
-        await client.send_message(message.channel, msg)
-    else:
-        pass
+class Roles(BotModule):
+    name = 'Roles'
+
+    description = 'Allow for the assignment and removal of roles.'
+
+    help_text = ''
+
+    trigger_string = '!role'  # String to listen for as trigger
+
+    async def parse_command(self, message, client):
+        server_roles = message.server.roles  # Grab a list of all roles as Role objects
+        server_roles_str = [x.name for x in server_roles]  # String-ify it into their names
+        msg = shlex.split(message.content)
+        role = [i for i, x in enumerate(server_roles_str) if x == msg[1]]  # Check where in the list the role is
+        role_to_assign = message.server.roles[role[0]]
+        if len(msg) != 1:
+            try:
+                if role_to_assign in message.author.roles:
+                    await client.remove_roles(message.author, role_to_assign)
+                    msg = ":ok_hand: Removed you from " + role_to_assign.name + " ."
+                else:
+                    await client.add_roles(message.author, role_to_assign)
+                    msg = ":ok_hand: Added you to " + role_to_assign.name + " ."
+            except discord.DiscordException:
+                msg = "I'm sorry " + message.author.name + " ,I'm afraid I can't do that."
+            await client.send_message(message.channel, msg)
+        else:
+            pass
