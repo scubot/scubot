719047cefe971c148059773806c08962b531482d
QuickRecon <aren.leishman@gmail.com>
2017-10-22 12:36:41 +0800

    convert to embed

--- modules/redditposts.py
+++ modules/redditposts.py
@@ -1,7 +1,6 @@
 import discord
 import requests
 import json
-from time import sleep
 
 redditPostsTriggerString = '!reddit'  # e.g. !convert
 
@@ -20,9 +19,60 @@ def get(sub):
             pass
 
 
-async def parse_reddit_post_command(message, client):
+def is_selfpost(data):
+    if "reddit.com" in data["url"] and not "np.reddit.com" in data["url"]:
+        return True
+    else:
+        return False
+
+
+def is_preview(data):
+    try:
+        i = data["preview"]
+        return True
+    except:
+        return False
+
+
+def determine(data):
+    if is_selfpost(data):
+        return 0
+    elif is_preview(data):
+        return 1
+    else:
+        return 3
+
+
+def truncate(text):
+    from textwrap import shorten
+    return shorten(text, 1600)
+
+
+def construct_embed(data, client):
+    det = determine(data)
+    if det == 0:
+        description = truncate(data["selftext"])
+        imageurl = None
+    elif det == 1:
+        description = data["url"]
+        imageurl = data["preview"]["images"][0]["source"]["url"]
+    elif det == 2:
+        description = None
+        imageurl = data["url"]
+    elif det == 3:
+        description = data["url"]
+        imageurl = None
+
+    embed = discord.Embed(title=data['title'], description=description, colour=0xDEADBF)
+    embed.set_author(name=data['author'], icon_url=client.user.default_avatar_url)
+    if imageurl is not None:
+        embed.set_image(url=imageurl)
+    return embed
+
+
+async def reddit_check_and_post_loop(message, client):
     data = get('scuba')
     most_recent_post = data[0]['data']
     if most_recent_post['id'] == lastPostId:
         return
-    await client.send_message(message.channel, most_recent_post['url'])
+    await client.send_message(message.channel, embed=construct_embed(most_recent_post, client))
