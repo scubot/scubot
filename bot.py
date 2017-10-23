8a35065065d15f0a2338a67678d610b9c5a44b72
QuickRecon <aren.leishman@gmail.com>
2017-10-23 20:10:27 +0800

    fixed blocking on loop

--- bot.py
+++ bot.py
@@ -1,32 +1,48 @@
 import discord
+
 from modules.units import *
+from modules.roles import *
+from modules.help import *
+from modules.status import *
 from modules.redditposts import *
+
+from modules.botModule import *
 
 client = discord.Client()
 
+BotModule.loaded_modules = [Units(), Roles(), Help(), Status(), RedditPost()]
 
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('!ping'):
-        msg = 'Pong! This bot is alive.'
-        await client.send_message(message.channel, msg)
-
-    elif message.content.startswith(unitsTriggerString):
-        await parse_units_command(message, client)
+    for bot_module in BotModule.loaded_modules:
+        if message.content.startswith(bot_module.trigger_string):
+            await bot_module.parse_command(message, client)
 
 
 @client.event
 async def on_ready():
-    print('Logged in as')
+    print('Login success. Your details:')
     print('User:', client.user.name)
     print('ID', client.user.id)
-    print('------')
-    await reddit_check_and_post_loop(client)  # TODO this locks the other modules
+    print('----------')
 
 
-tokenFile = open('token')
+print('scubot v' + BotModule.bot_version)
+try:
+    tokenFile = open('token')
+except FileNotFoundError:
+    print(
+        'Token not found. Please make sure you have a token file in this directory. Refer to README.md for details on getting a token.')
+    quit()
+print('Token found. Logging in...')
 token = tokenFile.read().replace('\n', '')
+
+for bot_module in BotModule.loaded_modules:
+    if bot_module.has_background_loop:
+        client.loop.create_task(bot_module.background_loop(client))
+
+
 client.run(token)
