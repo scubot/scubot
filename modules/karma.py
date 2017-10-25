f3875b1d9aed5f847b11846a27f7652e4c548b6c
Anson <git@anson-cheung.com>
2017-10-25 23:21:35 +0800

    Add logic and code for database operations (untested)

--- modules/karma.py
+++ modules/karma.py
@@ -10,12 +10,28 @@ class Karma(BotModule):
 
         trigger_string = '!reddit'
 
+        module_db = 'karma.json'
+
+        module_version = '0.1.0'
+
         listen_for_reaction = True
 
         async def parse_command(self, message, client):
             pass
 
         async def on_reaction(self, reaction, client):
-            print("karma_action triggered")
-            msg = "I saw that!" + reaction.message.author.name + reaction.emoji
-            await client.send_message(reaction.message.channel, msg)
+            target_user = self.module_db.Query()
+            if self.module_db.get(target_user.userid == reaction.message.author.id) == None:
+                self.module_db.insert({'userid': reaction.message.author.id, 'karma': 1})
+
+                msg = 'New entry for ' + reaction.message.author.id + ' added.'
+                await client.send_message(reaction.message.channel, msg)
+            else:
+                new_karma = self.module_db.get(target_user.userid == reaction.message.author.id)['karma'] + 1
+                self.module_db.update({'karma': new_karma}, target_user.userid == reaction.message.author.id)
+
+                msg = 'Karma for ' + reaction.message.author.id + ' updated to ' + new_karma
+                await client.send_message(reaction.message.channel, msg)
+
+            #msg = "I saw that!" + reaction.message.author.name + reaction.emoji
+            #await client.send_message(reaction.message.channel, msg)
