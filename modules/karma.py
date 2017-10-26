8c11106d499b5cbbdf6e87c7b325ee7df675ac00
suclearnub <suclearnub00@gmail.com>
2017-10-26 09:56:43 +0800

    Finalize basic working karma module

--- modules/karma.py
+++ modules/karma.py
@@ -9,7 +9,7 @@ class Karma(BotModule):
 
         help_text = 'This module has no callable functions'
 
-        trigger_string = '!reddit'
+        trigger_string = '!karma'
 
         module_db = 'karma.json'
 
@@ -18,21 +18,24 @@ class Karma(BotModule):
         listen_for_reaction = True
 
         async def parse_command(self, message, client):
-            pass
+            target_user = Query()
+            user_karma = self.module_db.get(target_user.userid == message.author.id)['karma']
+            msg = message.author.name + "'s karma: " + str(user_karma) + '.'
+            await client.send_message(message.channel, msg)
 
         async def on_reaction(self, reaction, client):
             target_user = Query()
             if self.module_db.get(target_user.userid == reaction.message.author.id) == None:
                 self.module_db.insert({'userid': reaction.message.author.id, 'karma': 1})
 
-                msg = 'New entry for ' + reaction.message.author.id + ' added.'
-                await client.send_message(reaction.message.channel, msg)
+                #msg = 'New entry for ' + reaction.message.author.id + ' added.'
+                #await client.send_message(reaction.message.channel, msg)
             else:
                 new_karma = self.module_db.get(target_user.userid == reaction.message.author.id)['karma'] + 1
                 self.module_db.update({'karma': new_karma}, target_user.userid == reaction.message.author.id)
 
-                msg = 'Karma for ' + reaction.message.author.id + ' updated to ' + str(new_karma)
-                await client.send_message(reaction.message.channel, msg)
+                #msg = 'Karma for ' + reaction.message.author.id + ' updated to ' + str(new_karma)
+                #await client.send_message(reaction.message.channel, msg)
 
             #msg = "I saw that!" + reaction.message.author.name + reaction.emoji
             #await client.send_message(reaction.message.channel, msg)
