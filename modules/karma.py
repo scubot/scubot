612eee740e0527f0f5c3becdd7a16f8a08ed6996
Anson <git@anson-cheung.com>
2018-11-24 23:26:36 +0000

    Add karma rank scrolling (#47)
    * Include reaction-scroll object into default modules
    
    * Add scrollable karma ranking into karma module
    
    * Update karma module version

--- modules/karma.py
+++ modules/karma.py
@@ -2,6 +2,26 @@ import discord
 from modules.botModule import *
 import shlex
 import time
+import modules.reactionscroll as rs
+
+
+class KarmaScrollable(rs.Scrollable):
+    async def preprocess(self, client, module_db):
+        ranked = sorted(module_db.all(), key=lambda k: k['karma'])[::-1]
+        ret = []
+        for item in ranked:
+            user = discord.utils.get(client.get_all_members(), id=item['userid'])
+            if not user:
+                ret.append(["User not found...", item['karma']])
+            else:
+                ret.append([user.name, item['karma']])
+        return ret
+
+    async def refresh(self, client, module_db):
+        self.processed_data.clear()
+        self.embeds.clear()
+        self.processed_data = await self.preprocess(client, module_db)
+        self.create_embeds()
 
 
 class Karma(BotModule):
@@ -15,18 +35,40 @@ class Karma(BotModule):
 
         trigger_string = 'karma'
 
-        module_version = '1.1.0'
+        module_version = '1.2.0'
 
         listen_for_reaction = True
 
         up_react = [':star:', '⭐', 'waitwhat']  # apparently some return as unicode emoji
         down_react = [':thumbsdown:', '👎', 'lionfish']
 
-        ranking_number = 5
-
-        ranking_embed_colour = 0xc0fefe
-
         cooldown_time = 30
+
+        message_returns = []
+
+        def __init__(self):
+            super(Karma, self).__init__()
+            self.scroll = KarmaScrollable(limit=5, color=0xc0fefe, table=self.module_db, title="Top users with karma",
+                                          inline=False)
+
+        async def contains_returns(self, message):
+            for x in self.message_returns:
+                if message.id == x[0].id:
+                    return True
+            return False
+
+        async def find_pos(self, message):
+            for x in self.message_returns:
+                if message.id == x[0].id:
+                    return x[1]
+
+        async def update_pos(self, message, ty):
+            for x in self.message_returns:
+                if message.id == x[0].id:
+                    if ty == 'next':
+                        x[1] += 1
+                    if ty == 'prev':
+                        x[1] -= 1
 
         def cooled_down(self, userid):
             target_user = Query()
@@ -52,20 +94,11 @@ class Karma(BotModule):
                     msg = "[:ok_hand:] Your karma has been reset to 0."
                     await client.send_message(message.channel, msg)
                 elif msg[1] == 'rank':
-                    pos = 1
-                    text = ''
-                    ranked = sorted(self.module_db.all(), key=lambda k: k['karma'])[::-1]
-                    ranked = ranked[:5]
-                    for entry in ranked:
-                        user_entry = message.server.get_member(entry['userid'])
-                        if user_entry is None:
-                            username = 'Unknown user'
-                        else:
-                            username = user_entry.name
-                        text += str(pos) + '     ' + username + ': ' + str(entry['karma']) + '\n'
-                        pos += 1
-                    embed = discord.Embed(title='Top ' + str(self.ranking_number) + ' users with karma', description=text, colour=self.ranking_embed_colour)
-                    await client.send_message(message.channel, embed=embed)
+                    await self.scroll.refresh(client, self.module_db)
+                    m_ret = await client.send_message(message.channel, embed=self.scroll.initial_embed())
+                    self.message_returns.append([m_ret, 0])
+                    await client.add_reaction(m_ret, "⏪")
+                    await client.add_reaction(m_ret, "⏩")
                 else:
                     pass
             else:
@@ -76,38 +109,54 @@ class Karma(BotModule):
                 await client.send_message(message.channel, msg)
 
         async def on_reaction_add(self, reaction, client, user):
-            if self.cooled_down(user.id):
-                cooldown = self.module_db.table('cooldown')
-                time_now = int(time.time())
+            react_text = reaction.emoji
+            if type(reaction.emoji) is not str:
+                react_text = reaction.emoji.name
+            if react_text == "⏩" or react_text == "⏪":
+                if not await self.contains_returns(reaction.message):
+                    return 0
+                pos = await self.find_pos(reaction.message)
                 react_text = reaction.emoji
-
                 if type(reaction.emoji) is not str:
                     react_text = reaction.emoji.name
+                if react_text == "⏩":
+                    embed = self.scroll.next(current_pos=pos)
+                    await client.edit_message(reaction.message, embed=embed)
+                    await self.update_pos(reaction.message, 'next')
+                if react_text == "⏪":
+                    embed = self.scroll.previous(current_pos=pos)
+                    await client.edit_message(reaction.message, embed=embed)
+                    await self.update_pos(reaction.message, 'prev')
 
-                target_user = Query()
-                rlist = []
+            else:
+                if self.cooled_down(user.id):
+                    cooldown = self.module_db.table('cooldown')
+                    time_now = int(time.time())
 
-                for x in reaction.message.reactions[
-                         :-1]:  # Check if person who reacted has already reacted to this message
-                    for u in await client.get_reaction_users(x):
-                        rlist.append(u)
+                    target_user = Query()
+                    rlist = []
 
-                if user not in rlist and reaction.message.author != user:  # DISABLE DURING DEVELOPMENT
-                    if self.module_db.get(target_user.userid == reaction.message.author.id) is None:
-                        self.module_db.insert({'userid': reaction.message.author.id, 'karma': 1})
+                    for x in reaction.message.reactions[
+                             :-1]:  # Check if person who reacted has already reacted to this message
+                        for u in await client.get_reaction_users(x):
+                            rlist.append(u)
 
-                    if react_text in self.up_react:
-                        new_karma = self.module_db.get(target_user.userid == reaction.message.author.id)['karma'] + 1
-                        self.module_db.update({'karma': new_karma}, target_user.userid == reaction.message.author.id)
+                    if user not in rlist and reaction.message.author != user:  # DISABLE DURING DEVELOPMENT
+                        if self.module_db.get(target_user.userid == reaction.message.author.id) is None:
+                            self.module_db.insert({'userid': reaction.message.author.id, 'karma': 1})
 
-                    if react_text in self.down_react:
-                        new_karma = self.module_db.get(target_user.userid == reaction.message.author.id)['karma'] - 1
-                        self.module_db.update({'karma': new_karma}, target_user.userid == reaction.message.author.id)
+                        if react_text in self.up_react:
+                            new_karma = self.module_db.get(target_user.userid == reaction.message.author.id)['karma'] + 1
+                            self.module_db.update({'karma': new_karma}, target_user.userid == reaction.message.author.id)
 
-                    cooldown.update({'lastreact': time_now}, target_user.userid == user.id)
+                        if react_text in self.down_react:
+                            new_karma = self.module_db.get(target_user.userid == reaction.message.author.id)['karma'] - 1
+                            self.module_db.update({'karma': new_karma}, target_user.userid == reaction.message.author.id)
 
-                else:
-                    pass
+                        cooldown.update({'lastreact': time_now}, target_user.userid == user.id)
+
+                    else:
+                        pass
 
         async def on_reaction_remove(self, reaction, client, user):
             react_text = reaction.emoji
