6292dc54600f055ee4d2ae050d93d99d2d7b80f0
Anson <git@anson-cheung.com>
2019-05-04 01:16:35 +0100

    Rework status to look better

--- modules/status.py
+++ modules/status.py
@@ -6,7 +6,7 @@ import json
 
 class Status(commands.Cog):
     def __init__(self, bot):
-        self.version = "2.0.0"
+        self.version = "2.1.0"
         self.bot = bot
         self.start_time = time.time()
 
@@ -17,12 +17,14 @@ class Status(commands.Cog):
         days, hours = divmod(hours, 24)
         return days, hours, minutes, seconds
 
-    @staticmethod
-    def loaded_modules(bot):
+    def loaded_modules(self):
         load = ''
-        for key,value in bot.cogs.items():
-            load += key + "(" + value.version + ")" + ", "
+        for key, value in self.bot.cogs.items():
+            load += key + " (" + value.version + ")" + ", "
         return load[:-2]
+
+    def get_latency(self):
+        return "{:0.2f}ms".format((self.bot.latency * 1000))
 
     @commands.command()
     async def status(self, ctx):
@@ -30,12 +32,13 @@ class Status(commands.Cog):
         uptime_string = [str(round(x, 0))[:-2] for x in self.uptime_convert(uptime)]
         uptime_string = uptime_string[0] + 'd ' + uptime_string[1] + 'h ' + uptime_string[2] + 'm ' + uptime_string[
             3] + 's'
-        module_string = self.loaded_modules(self.bot)
+        module_string = self.loaded_modules()
         embed = discord.Embed(title="Status", description="Status and information about this bot", color=0x008080)
-        embed.add_field(name="Uptime", value=uptime_string, inline=True)
-        embed.add_field(name="Loaded Modules", value=module_string, inline=True)
-        embed.add_field(name="Bot version", value=self.bot.version, inline=True)
-        embed.add_field(name="Donate", value="https://www.paypal.me/theansoncheung", inline=True)
+        embed.add_field(name="Uptime", value=uptime_string, inline=False)
+        embed.add_field(name="Loaded Modules", value=module_string, inline=False)
+        embed.add_field(name="Bot version", value=self.bot.version, inline=False)
+        embed.add_field(name="Latency", value=self.get_latency(), inline=False)
+        embed.add_field(name="Donate", value="https://www.paypal.me/theansoncheung", inline=False)
         embed.set_footer(text="Powered by scubot: https://github.com/scubot/scubot")
         await ctx.send(embed=embed)
 
