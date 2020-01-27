b6aa9fb339f1718795a213f3c9557c20ead130ea
tandemdude <olijdthompson@gmail.com>
2020-01-27 17:52:21 +0000

    Fix wonky error handling for loading errors

--- modules/loader.py
+++ modules/loader.py
@@ -6,6 +6,21 @@ class Loader(commands.Cog):
         self.version = "1.0.1"
         self.bot = bot
 
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, commands.errors.CommandInvokeError):
+            print(f"[WARN] {str(error)}")
+            if isinstance(error.original, commands.errors.ExtensionNotLoaded):
+                return await ctx.send("[!] Module not already loaded.")
+            elif isinstance(error.original, commands.errors.ExtensionAlreadyLoaded):
+                return await ctx.send("[!] Module already loaded.")
+            elif isinstance(error.original, commands.errors.ExtensionNotFound):
+                print("YEEET")
+                return await ctx.send("[!] Module not found!")
+            elif isinstance(error.original, commands.errors.NoEntryPointError):
+                return await ctx.send("[!] Module has no entry point!")
+            elif isinstance(error.original, commands.errors.ExtensionFailed):
+                return await ctx.send("[!] Module setup encountered execution error! Check logs for details.")
+
     @commands.has_any_role('Moderators', 'Admin', 'devs')
     @commands.command()
     async def reload(self, ctx, *, module: str):
@@ -26,20 +41,6 @@ class Loader(commands.Cog):
         print(f"[LOAD] Unloading {module}")
         self.bot.unload_extension(module)
         await ctx.send(f"[:ok_hand:] Module {module} was unloaded.")
-
-    async def cog_command_error(self, ctx, error):
-        if isinstance(error, commands.errors.CommandInvokeError):
-            print(f"[WARN] {str(error)}")
-        elif isinstance(error, commands.ExtensionNotLoaded):
-            await ctx.send("[!] Module not already loaded.")
-        elif isinstance(error, commands.ExtensionAlreadyLoaded):
-            await ctx.send("[!] Module already loaded.")
-        elif isinstance(error, commands.ExtensionNotFound):
-            await ctx.send("[!] Module not found!")
-        elif isinstance(error, commands.NoEntryPointError):
-            await ctx.send("[!] Module has no entry point!")
-        elif isinstance(error, commands.ExtensionFailed):
-            await ctx.send("[!] Module setup encountered execution error! Check logs for details.")
 
 
 def setup(bot):
