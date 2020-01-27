6b4eca7843b8e1cd58b3e51ee0a9f5093631fe87
tandemdude <olijdthompson@gmail.com>
2020-01-27 11:28:48 +0000

    Improve role handling using a role converter instead of discord.utils.find

--- modules/role.py
+++ modules/role.py
@@ -1,30 +1,28 @@
-import time
-
 from discord.ext import commands
 import discord
-from discord.utils import find
-from tinydb import *
 
 class Role(commands.Cog):
     def __init__(self, bot):
-        self.version = "2.0.0"
+        self.version = "2.0.1"
         self.bot = bot
 
     @commands.command()
-    async def role(self, ctx, *, role: str = None):
-        role_to_assign = find(lambda r: r.name == role, ctx.message.guild.roles)
+    async def role(self, ctx, *, role: discord.Role = None):
+        if role is None:
+            return await ctx.send("[!] You did not specify a role.")
         try:
-            await ctx.author.add_roles(role_to_assign)
-            await ctx.send("[:ok_hand:] Assigned you to " + str(role_to_assign) + " .")
+            await ctx.author.add_roles(role)
+            await ctx.send(f"[:ok_hand:] Assigned you to {role.name}.")
         except discord.Forbidden:
             await ctx.send("[!] Could not assign you to that role.")
 
     @commands.command()
-    async def derole(self, ctx, *, role: str = None):
-        role_to_remove = find(lambda r: r.name == role, ctx.message.guild.roles)
+    async def derole(self, ctx, *, role: discord.Role = None):
+        if role is None:
+            return await ctx.send("[!] You did not specify a role.")
         try:
-            await ctx.author.remove_roles(role_to_remove)
-            await ctx.send("[:ok_hand:] Removed you from " + str(role_to_remove) + " .")
+            await ctx.author.remove_roles(role)
+            await ctx.send(f"[:ok_hand:] Removed you from {role.name}.")
         except discord.Forbidden:
             await ctx.send("[!] Could not remove you from that role.")
 
