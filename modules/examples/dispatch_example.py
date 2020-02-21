4c57e30ad69fadad1d84fd59d6d5f7d5c92408fa
Anson <git@anson-cheung.com>
2020-02-21 16:49:04 +0000

    Refactor dispatch_example.py

--- modules/examples/dispatch_example.py
+++ modules/examples/dispatch_example.py
@@ -12,14 +12,14 @@ class DispatchExample(commands.Cog):
 
     @staticmethod
     def create_embedchain(pages: int) -> EmbedChain:
-        return EmbedChain([EmbedEntry("Hello", x) for x in range(1, pages+1)], limit=1, color=0x64c891, title="Dispatch Example",
+        return EmbedChain([EmbedEntry("Hello", x) for x in range(1, pages+1)], limit=1, color=0x64c891,
+                          title="Dispatch Example",
                           inline=True)
 
     @commands.group(invoke_without_command=True)
     async def dispatch_example(self, ctx, pages: int = 5):
-        embed = self.create_embedchain(pages)
-        sent_message = await ctx.send(embed=embed.current())
-        await self.dispatcher.register(sent_message, embed)
+        embed: EmbedChain = self.create_embedchain(pages)
+        await self.dispatcher.register(await ctx.send(embed=embed.current()), embed)
 
 
 def setup(bot):
